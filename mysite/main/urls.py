f6193338f755d33d157d33552f612ab73c23eb5d
haydentinker <haydentinker613@gmail.com>
2023-09-13 10:59:01 -0700

    Basic sql database setup with todolist

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     path("", views.index, name="index"),
-    path("v1/",views.v1,name='v1')
+    path('item/<int:id>',views.v1,name="v1"),
+    path('list/<int:id>',views.v2,name="v2")
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import ToDoList, Item
 
 def index(response):
     return HttpResponse("hello")
 
-def v1(response):
-    return HttpResponse("Hello world")
+def v1(response,id):
+    ls=ToDoList.objects.get(id=id)
+    return HttpResponse("%s" % ls.item_set.get())
+def v2(response,id):
+    ls=ToDoList.objects.get(id=id)
+    return HttpResponse("%s" % ls.name)
